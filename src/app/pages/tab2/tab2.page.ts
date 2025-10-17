974efe52f884c0dbacd104d4f42a2307e6aff2d7
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-10-17 15:18:02 +0100

    feat: animation

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
@@ -8,7 +14,13 @@ import {
   IonToolbar,
   IonButtons,
   IonMenuButton,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonCardContent,
 } from '@ionic/angular/standalone';
+import type { Animation } from '@ionic/angular/standalone';
+import { AnimationController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
@@ -24,10 +36,38 @@ import {
     CommonModule,
     FormsModule,
     IonMenuButton,
+    IonIcon,
+    IonButton,
+    IonCard,
+    IonCardContent,
   ],
 })
-export class Tab2Page implements OnInit {
-  constructor() {}
+export class Tab2Page implements OnInit, AfterViewInit {
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
+  private animation!: Animation;
+
+  constructor(private animationCtrl: AnimationController) {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(2000)
+      .iterations(Infinity)
+      .delay(1000)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.5)', opacity: '0.5' },
+        { offset: 1, transform: 'scale(1)', opacity: '1' },
+      ]);
+  }
+
+  play() {
+    if (this.animation) {
+      this.animation.play();
+    }
+  }
 }
